54d36c103ad7ec82269662d4d12cfca520bb029c
Parshant Rajput <54788475+Parshant679@users.noreply.github.com>
2024-03-03 17:17:16 +0530

    " basic crud operations in node js"

---
+++ DATA.js
@@ -0,0 +1,19 @@
+const userData = [
+  {
+    id: 1,
+    name: "titan",
+    class: "first",
+  },
+  {
+    id: 2,
+    name: "Oneplus",
+    class: "second",
+  },
+  {
+    id: 1,
+    name: "Oppo",
+    class: "third",
+  },
+];
+
+module.exports = userData;
